d61b63acdc3c0bde9c2f48f966354f5c91d674d7
demnofocus <47452568+demnofocus@users.noreply.github.com>
2020-08-17 01:43:17 +0530

    created: HomePage.java, LoginPage.java, SecureAreaPage.java
    commit contains chapter 4.1 work

--- TestAutomation/webdriver_java/src/test/java/base/BaseTests.java
+++ TestAutomation/webdriver_java/src/test/java/base/BaseTests.java
@@ -2,11 +2,13 @@ package base;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver",
@@ -18,28 +20,32 @@ public class BaseTests {
         //iphoneX view
         //driver.manage().window().setSize(new Dimension(375, 812));
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println("No.of elements with the tag 'a': " + links.size());
+        /*List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println("No.of elements with the tag 'a': " + links.size());*/
 
         //not available
-        try {
+        /*try {
             WebElement inputsLink = driver.findElement(By.linkText("Angie"));
             inputsLink.click();
         }catch (Exception e){
             System.out.println("Error: " + e.getMessage());
-        }
+        }*/
 
         /*
             Exercise
              ->the internet -> shifting content -> menu elements -> print no of menu elements
          */
-        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
+        /*WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
         shiftingContent.click();
         WebElement menu = driver.findElement(By.className("example"));
         List<WebElement> menuElements = menu.findElements(By.tagName("a"));
         System.out.println("\nNo.of menu options = " + menuElements.size());
+        */
+        //System.out.println(driver.getTitle());
 
-        System.out.println(driver.getTitle());
+        //chapter 04
+        homePage = new HomePage(driver);
+
         //driver.close();     //close window
         driver.quit();  //end session
     }
