747b51509014b3e36203ee7326c7e5f911c59ab6
demnofocus <47452568+demnofocus@users.noreply.github.com>
2020-08-17 20:30:47 +0530

    created: DropdownPage.java, DropdownTests.java.
    commit contains new class for chapter 4.3 and code of the chapter

--- TestAutomation/webdriver_java/src/test/java/base/BaseTests.java
+++ TestAutomation/webdriver_java/src/test/java/base/BaseTests.java
@@ -2,15 +2,15 @@ package base;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
-
-import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
-    public void setUp(){
+    /*public void setUp(){
         System.setProperty("webdriver.chrome.driver",
                 "resources/chromeDriver.exe");
         driver = new ChromeDriver();
@@ -20,38 +20,52 @@ public class BaseTests {
         //iphoneX view
         //driver.manage().window().setSize(new Dimension(375, 812));
 
-        /*List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println("No.of elements with the tag 'a': " + links.size());*/
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println("No.of elements with the tag 'a': " + links.size());
 
         //not available
-        /*try {
+        try {
             WebElement inputsLink = driver.findElement(By.linkText("Angie"));
             inputsLink.click();
         }catch (Exception e){
             System.out.println("Error: " + e.getMessage());
-        }*/
+        }
 
-        /*
+        *//*
             Exercise
              ->the internet -> shifting content -> menu elements -> print no of menu elements
-         */
-        /*WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
+         *//*
+        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
         shiftingContent.click();
         WebElement menu = driver.findElement(By.className("example"));
         List<WebElement> menuElements = menu.findElements(By.tagName("a"));
         System.out.println("\nNo.of menu options = " + menuElements.size());
-        */
-        //System.out.println(driver.getTitle());
 
-        //chapter 04
-        homePage = new HomePage(driver);
+        System.out.println(driver.getTitle());
 
         //driver.close();     //close window
         driver.quit();  //end session
+    }*/
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver",
+                "resources/chromeDriver.exe");
+        driver = new ChromeDriver();
+        //launch web app
+        driver.get("https://the-internet.herokuapp.com/");
+
+        homePage = new HomePage(driver);
+
     }
 
-    public static void main(String[] args){
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    /*public static void main(String[] args){
         BaseTests baseTests = new BaseTests();
         baseTests.setUp();
-    }
+    }*/
 }
