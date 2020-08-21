9915f5c2035a0915a6372190183d4dc4db2f2207
demnofocus <47452568+demnofocus@users.noreply.github.com>
2020-08-22 01:16:11 +0530

    created: KeyPressesPage.java, KeyTests.class
    commit contains chapter 6 work

--- TestAutomation/webdriver_java/src/main/java/pages/HoversPage.java
+++ TestAutomation/webdriver_java/src/main/java/pages/HoversPage.java
@@ -28,9 +28,27 @@ public class HoversPage {
 
     public class FigureCaption{
         private WebElement caption;
+        private By header = By.tagName("h5");
+        private By link = By.tagName("a");
 
         public FigureCaption(WebElement caption){
             this.caption = caption;
+        }
+
+        public boolean isCaptionDisplayed(){
+            return caption.isDisplayed();
+        }
+
+        public String getTitle(){
+            return caption.findElement(header).getText();
+        }
+
+        public String getLink(){
+            return caption.findElement(link).getAttribute("href");
+        }
+
+        public String getLinkText(){
+            return caption.findElement(link).getText();
         }
     }
 }
